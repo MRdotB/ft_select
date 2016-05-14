6ac4854b14c0a9b7b79fb968fe08f4b4308faf50
mrdotb <baptiste.chaleil@gmail.com>
2016-05-14 21:43:44 +0200

    Update main

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: bchaleil <hello@baptistechaleil.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/13 16:45:23 by bchaleil          #+#    #+#             */
-/*   Updated: 2016/05/13 18:08:34 by bchaleil         ###   ########.fr       */
+/*   Updated: 2016/05/14 21:33:58 by bchaleil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,12 @@ int	main(int ac, char **av)
 {
 	if (ft_select_init(ac) == -1)
 		return (1);
-	if (av)
+	if (!av)
 		return (1);
+	handle_signal(0);
+	cmdput("ti");
+	cmdput("cl");
+	render();
+	listen_keystroke();
 	return (0);
 }
