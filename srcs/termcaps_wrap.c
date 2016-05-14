84429c2e739fe0e882d8ab2cca80a4b620ad9e8a
mrdotb <baptiste.chaleil@gmail.com>
2016-05-14 21:43:29 +0200

    wrap termcaps fn

---
+++ srcs/termcaps_wrap.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   termcaps_wrap.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bchaleil <hello@baptistechaleil.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/05/13 22:13:51 by bchaleil          #+#    #+#             */
+/*   Updated: 2016/05/14 16:14:39 by bchaleil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_select.h"
+
+void	cmdput(char *area)
+{
+	char	*ptr;
+
+	if ((ptr = tgetstr(area, NULL)) != NULL)
+		ft_putstr_fd(ptr, isatty(STDOUT_FILENO));
+	return ;
+}
+
+
+int		cmdgoto(int x, int y)
+{
+	char	*ptr;
+	char	*ptr2;
+
+	if ((ptr = tgetstr("cm", NULL)) != NULL)
+	{
+		if ((ptr2 = tgoto(ptr, x, y)) != NULL)
+			ft_putstr_fd(ptr2, isatty(STDOUT_FILENO));
+	}
+	return (1);
+}
