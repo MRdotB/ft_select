e8d15868d1fbe85a39edad2ceda3568fd1001a8d
mrdotb <baptiste.chaleil@gmail.com>
2016-05-15 23:11:51 +0200

    Fourtout

---
+++ srcs/slt_elm.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   slt_elm.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bchaleil <hello@baptistechaleil.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/05/14 22:21:13 by bchaleil          #+#    #+#             */
+/*   Updated: 2016/05/15 23:10:15 by bchaleil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_select.h"
+
+static t_slt_el	create_entry(char *str)
+{
+	t_slt_el el;
+
+	el.str = ft_strdup(str);
+	el.selected = 0;
+	el.current = 0;
+	el.x = 0;
+	el.y = 0;
+	return (el);
+}
+
+void			entry_init(t_dlist **head, char **av)
+{
+	t_slt_el	el;
+
+	while (*av)
+	{
+		el = create_entry(*av);
+		ft_dlstpush(head, ft_dlstnew(&el, sizeof(&el))); 
+		av++;
+	}
+}
