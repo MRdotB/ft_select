25ac5fefe9aaad2f6ce0a420a8dcbec5d7be623f
mrdotb <baptiste.chaleil@gmail.com>
2016-05-15 23:13:12 +0200

    temp main

--- srcs/main.c
+++ srcs/main.c
@@ -6,22 +6,32 @@
 /*   By: bchaleil <hello@baptistechaleil.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/13 16:45:23 by bchaleil          #+#    #+#             */
-/*   Updated: 2016/05/14 21:33:58 by bchaleil         ###   ########.fr       */
+/*   Updated: 2016/05/15 23:13:03 by bchaleil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_select.h"
 
+void	checkcontent(t_dlist *el)
+{
+	ft_putendl(((t_slt_el*)el->content)->str);
+	ft_putnbrnl(ft_strlen(((t_slt_el*)el->content)->str));
+	ft_putnbrnl(((t_slt_el*)el->content)->x);
+	ft_putnbrnl(((t_slt_el*)el->content)->y);
+}
+
 int	main(int ac, char **av)
 {
+	t_dlist		*head;
+
+	head = NULL;
 	if (ft_select_init(ac) == -1)
 		return (1);
-	if (!av)
-		return (1);
+	entry_init(&head, av + 1);
 	handle_signal(0);
 	cmdput("ti");
 	cmdput("cl");
-	render();
+	render(head);
 	listen_keystroke();
 	return (0);
 }
