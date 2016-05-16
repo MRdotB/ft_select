71950f9c4a22e2b6006e58214be05073e9dcf7fa
mrdotb <baptiste.chaleil@gmail.com>
2016-05-16 22:17:45 +0200

    listen keystroke add array of function

--- srcs/listen_keystroke.c
+++ srcs/listen_keystroke.c
@@ -6,7 +6,7 @@
 /*   By: bchaleil <hello@baptistechaleil.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/13 18:02:22 by bchaleil          #+#    #+#             */
-/*   Updated: 2016/05/16 00:13:35 by bchaleil         ###   ########.fr       */
+/*   Updated: 2016/05/16 21:59:46 by bchaleil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 
 static int		ft_chrmatch(char *str)
 {
-	static ssize_t	match[] = {CLF, SUP, CHT, DEL, LEF, RIG, UPP, DOW, CLEF, CRIG, CUPP, CDOW, END, HOM, ESC, NUL};
+	static ssize_t	match[] = {CLF, SUP, DEL, LEF, RIG, UPP, DOW, SPA, ESC, NUL};
 	int	i;
 
 	i = 0;
-	printf("\nbuf = %lx\n", ((ssize_t *)(str))[0]);
+//	printf("\nbuf = %lx\n", ((ssize_t *)(str))[0]);
 	while (match[i])
 	{
 //		printf("\nmatch = %lx\n", match[i]);
@@ -29,29 +29,25 @@ static int		ft_chrmatch(char *str)
 	return (0);
 }
 
-int	my_outc(int c)
-{
-	return write(1, &c, 1);
-}
-
-
-void	listen_keystroke(t_dlist *lst)
+void	listen_keystroke(t_dlist **lst, t_dlist *head)
 {
 	char	buffer[8];
 	int		el;
-	char	*touche[] = {"CLF", "SUP", "CHT", "DEL", "le", "nd", "up", "do", "le", "CRIG", "CUPP", "CDOW", "END", "HOM", "ESC", "NUL"};
-
+	char	*touche[] = {"CLF", "SUP", "DEL", "LEF", "RIG", "UPP", "DOW", "SPA", "ESC", "NUL"};
+	static void (*f[])(t_dlist **lst) = { lst_move_up, lst_move_up, lst_move_up, lst_move_left,
+		lst_move_right, lst_move_up, lst_move_down, lst_select, lst_move_down, lst_move_down};
 	ft_memset(buffer, 0, 8);
 	while (read(0, buffer, 8) != -1)
 	{
 		el = ft_chrmatch(buffer);
 		if (ft_strcmp("ESC", touche[el]) == 0)
 		{
-			exit(0);
+			handle_signal(0);
 		}
 		term_clear();
-		render(lst);
-		ft_putendl(touche[el]);
+		f[el](lst);
+		render(head, 0);
+//		ft_putendl(touche[el]);
 		ft_memset(buffer, 0, 8);
 	}
 }
