b0af4d3d292704b79f92e49d92410702a74a5b6c
mrdotb <baptiste.chaleil@gmail.com>
2016-05-18 01:29:27 +0200

    handle sigstp sigcont

--- srcs/signal.c
+++ srcs/signal.c
@@ -6,7 +6,7 @@
 /*   By: bchaleil <hello@baptistechaleil.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/14 21:39:15 by bchaleil          #+#    #+#             */
-/*   Updated: 2016/05/16 22:14:47 by bchaleil         ###   ########.fr       */
+/*   Updated: 2016/05/18 01:23:56 by bchaleil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,52 +14,53 @@
 
 void	catch_signal(void)
 {
-//	MAC
-//	signal(SIGEMT, handle_signal);
-	signal(SIGHUP, handle_signal);
-	signal(SIGQUIT, handle_signal);
-	signal(SIGINT, handle_signal);
-	signal(SIGBUS, handle_signal);
-	signal(SIGFPE, handle_signal);
-	signal(SIGSYS, handle_signal);
-	signal(SIGABRT, handle_signal);
-	signal(SIGALRM, handle_signal);
-	signal(SIGCONT, handle_signal);
+	signal(SIGBUS, &handle_signal);
+	//signal(SIGEMT, &handle_signal);
+	signal(SIGFPE, &handle_signal);
+	signal(SIGHUP, &handle_signal);
+	signal(SIGINT, &handle_signal);
+	signal(SIGSYS, &handle_signal);
+	signal(SIGABRT, &handle_signal);
+	signal(SIGALRM, &handle_signal);
+	signal(SIGCONT, &handle_signal);
+	signal(SIGKILL, &handle_signal);
 	signal(SIGPIPE, SIG_DFL);
-//	signal(SIGSEGV, handle_signal);
-	signal(SIGTERM, handle_signal);
-	signal(SIGTTOU, handle_signal);
-	signal(SIGTSTP, handle_signal);
-	signal(SIGUSR1, handle_signal);
-	signal(SIGUSR2, handle_signal);
-	signal(SIGXCPU, handle_signal);
-	signal(SIGXFSZ, handle_signal);
-	signal(SIGWINCH, handle_signal);
+	signal(SIGQUIT, &handle_signal);
+	signal(SIGSEGV, &handle_signal);
+	signal(SIGTERM, &handle_signal);
+	signal(SIGTTOU, &handle_signal);
+	signal(SIGSTOP, &handle_signal);
+	signal(SIGTSTP, &handle_signal);
+	signal(SIGUSR1, &handle_signal);
+	signal(SIGUSR2, &handle_signal);
+	signal(SIGXCPU, &handle_signal);
+	signal(SIGXFSZ, &handle_signal);
+	signal(SIGWINCH, &handle_signal);
 }
 
 void		handle_signal(int sig)
 {
 	static struct termios	*config;
 
-	if (config == NULL)
+	if (config == NULL || sig == SIGTSTP || sig == SIGCONT)
 	{
-		catch_signal();
-		cmdput("vi");
 		term_setup(&config);
+		if (sig == SIGTSTP)
+			term_restore(&config);
+		else if (sig == SIGCONT)
+		{
+			term_setup(&config);
+			term_clear();
+			render(NULL, 1);
+		}
 	}
-	else if (sig == SIGTSTP)
-		term_restore(&config);
-	else if (sig == SIGCONT)
-		ft_putendl("sigcount ctrl z");
-	else if (sig == SIGWINCH || sig == 11)
+	else if (sig == SIGWINCH)
 	{
 		term_clear();
 		render(NULL, 1);
 	}
 	else
 	{
-//	DEBUG
-//		ft_putnbrnl(sig);
 		cmdput("ve");
 		cmdput("te");
 		tcsetattr(STDIN_FILENO, TCSANOW, config);
